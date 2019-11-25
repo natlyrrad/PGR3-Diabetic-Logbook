ac30878fec94e224bc12726570e04d06e9220df1
yuuq-huang <56388680+yuuq-huang@users.noreply.github.com>
2019-11-25 23:54:25 +0000

    Add files via upload

---
+++ CreateTable.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+
+public class CreateTable{
+    public static void create(String requirement, Connection conn)  {
+            try {
+               PreparedStatement st = conn.prepareStatement(requirement);
+               st.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+        }
+
+    }}
