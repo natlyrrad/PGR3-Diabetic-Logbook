4bcab8f702f038c8a7dc81a73b2ea359e6c38f4b
senalkaw <senalkawickramaratne@hotmail.com>
2019-11-28 10:51:28 +0000

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,22 @@
 
 
-import drawingUI.details_page.DetailsUIController;
-import drawingUI.email_page.EmailUIController;
-
 import javax.swing.*;
-import java.awt.*;
-
-public class Main {
-
-    static GraphicsConfiguration gc; // Class field containing config info
-
-    public static void main(String[] args) {
-        JFrame email_frame= new JFrame(gc); // Create a new JFrame
-        email_frame.setSize(500,300);
-
-        EmailUIController uiemail = new EmailUIController(email_frame);
-
-        email_frame.setVisible(true);
-        // This next line closes the program when the frame is closed
-        email_frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 
-       //Here is the frame for the actual log itself
-        //create new frame
-        JFrame frame= new JFrame("Blood Sugar Level Input");
-        frame.setSize(700,300);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    public class Main {
+        public static void main(String[] args) {
+            //create new frame
+            JFrame frame= new JFrame("Blood Sugar Level Input");
+            frame.setSize(700,300);
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        //add panel to frame
-        EntryPanel mainPanel = new EntryPanel();
-        frame.getContentPane().add(mainPanel);
+            //add panel to frame
+            CompUI mainPanel = new CompUI();
+            frame.getContentPane().add(mainPanel);
 
-        frame.setVisible(true);
-
+            frame.setVisible(true);
+        }
     }
-}
+
+    
+
--- src/main/java/connectDB.java
+++ src/main/java/connectDB.java
@@ -1,27 +1,29 @@
-import java.sql.*;
 
-public class connectDB {
-    private static String url= "jdbc:postgresql://localhost:5432/postgres";//load the database
-    private static String username = "postgres";//user name for the database
-    private static String password = "<your password for the database>";//password for the database
-    private static Connection conn;
-    public static Connection connect() {
-        try {
-            Class.forName("org.postgresql.Driver"); //load the JDBC driver
-            System.out.println("ok");
-            conn = DriverManager.getConnection(url, username, password);
-            {
+    import java.sql.*;
 
-                if (conn != null) {
-                    System.out.println("Connected to the database!");
-                } else {
-                    System.out.println("Failed to make connection!");
-                }//get connection to database
+    public class connectDB {
+        private static String url= "jdbc:postgresql://localhost:5432/postgres";//load the database
+        private static String username = "postgres";//user name for the database
+        private static String password = "<your password for the database>";//password for the database
+        public static Connection conn;
+        public static Connection connect() {
+            try {
+                Class.forName("org.postgresql.Driver"); //load the JDBC driver
+                System.out.println("ok");
+                conn = DriverManager.getConnection(url, username, password);
+                {
+
+                    if (conn != null) {
+                        System.out.println("Connected to the database!");
+                    } else {
+                        System.out.println("Failed to make connection!");
+                    }//get connection to database
+                }
+            }catch (SQLException | ClassNotFoundException ex) {
+                System.out.println("Failed to create the database connection");
             }
-        }catch (SQLException | ClassNotFoundException ex) {
-            System.out.println("Failed to create the database connection");
-        }
-        return conn;
+            return conn;
 
+        }
     }
-}
+
