9de97ac3f660310285ae2f88885ed286b5d7cedb
senalkaw <senalkawickramaratne@hotmail.com>
2019-12-11 20:15:39 +0000

    Questionnaire

--- src/main/java/QuestPage/Questionnaire.java
+++ src/main/java/QuestPage/Questionnaire.java
@@ -1,5 +1,6 @@
 package QuestPage;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 import drawingUI.logPage.LogUIController;
 
 import javax.swing.*;
@@ -14,30 +15,16 @@ import javax.swing.*;
 public class Questionnaire extends JPanel {
 
     String[] options = {"1", "2", "3"};
+    JLabel[] questions =new JLabel[14];
     JButton back = new JButton("< Back");
 
+
     public Questionnaire() {
-        JPanel QN = new JPanel(new GridBagLayout());
+        GridBagLayout grid = new GridBagLayout();
+        this.setLayout(grid);
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.insets = new Insets(10, 10, 10, 10);
-
-
-        JLabel header = new JLabel("Questionnaire");
-        JLabel instr = new JLabel("1 = least likely and 3 = most likely ");
-        JLabel q1 = new JLabel("1. I check my blood sugar levels daily");
-        JLabel q2 = new JLabel("2. The food I choose to eat daily makes it easy to achieve optimal blood sugar levels.");
-        JLabel q3 = new JLabel("3. I take my diabetes medication (e. g. insulin, tablets) as prescribed");
-        JLabel q4 = new JLabel("4. I tend to eat foods rich in carbohydrates and sweets daily");
-        JLabel q5 = new JLabel("5. I tend to avoid diabetes-related doctors’ appointments.");
-        JLabel q6 = new JLabel("6. I strictly follow the dietary recommendations given by my doctor or diabetes specialist daily.");
-        JLabel q7 = new JLabel("7. Sometimes I have real ‘food binges’ (not triggered by hypoglycaemia).");
-        JLabel q8 = new JLabel("8. I do regular physical activity to achieve optimal blood sugar levels.");
-        JLabel q9 = new JLabel("9. I tend to miss my dose of diabetes");
-        JLabel q10 = new JLabel("10. I get good support system for it including  my doctors,family and friends");
-        JLabel q11 = new JLabel("11. I tend to smoke daily");
-        JLabel q12 = new JLabel("12. I have good diabetes self care");
-
+        constraints.insets = new Insets(10, 0, 10, 0);
 
         back.addActionListener(new ActionListener(){
             @Override
@@ -61,79 +48,67 @@ public class Questionnaire extends JPanel {
             }
         });
 
-        for (int i = 0; i < 12; i++) {
-            constraints.fill = GridBagConstraints.BOTH;
-            constraints.gridy = i;
-            for( int j = 0;j <14;j++) {
-                QN.add(new JComboBox(options));
-            }
+
+        questions[0]  = new JLabel("Questionnaire");
+        questions[1] = new JLabel("Instructions: choose 1 for least likely and choose 3 for most likely ");
+        questions[2] = new JLabel("1. I check my blood sugar levels daily");
+        questions[3]= new JLabel("2. The food I choose to eat daily makes it easy to achieve optimal blood sugar levels.");
+        questions[4] = new JLabel("3. I take my diabetes medication (e. g. insulin, tablets) as prescribed");
+        questions[5]= new JLabel("4. I tend to eat foods rich in carbohydrates and sweets daily");
+        questions[6] = new JLabel("5. I tend to avoid diabetes-related doctor appointments.");
+        questions[7] = new JLabel("6. I strictly follow the dietary recommendations given by my doctor or diabetes specialist daily.");
+        questions[8] = new JLabel("7. Sometimes I have real 'food binges' (not triggered by hypoglycaemia).");
+        questions[9] = new JLabel("8. I do regular physical activity to achieve optimal blood sugar levels.");
+        questions[10]= new JLabel("9. I tend to miss my dose of diabetes");
+        questions[11] = new JLabel("10. I get good support system for it including  my doctors, family and friends");
+        questions[12]= new JLabel("11. I tend to smoke daily");
+        questions[13] = new JLabel("12. I have good diabetes self care");
+
+        questions[0].setFont(new Font("Dialog", Font.BOLD, 16));
+        questions[0].setForeground(Color.BLUE);
+        questions[1].setFont(new Font("Dialog", Font.BOLD, 14));
+
+        //positioning of components
+        for (int i =  0; i < 14; i++) {
+            constraints.gridx = 0;
+            add(questions[i],constraints);
+            System.out.println(i);
         }
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        QN.add(header,constraints);
 
-        constraints.gridx = 0;
-        constraints.gridy = 2;
-        QN.add(instr,constraints);
+        for (int i =  2; i < 14; i++) {
+            constraints.gridx = 1;
+            constraints.gridy = i;
+            add(new JComboBox(options), constraints);
+        }
 
-        constraints.gridx = 0;
-        constraints.gridy = 3;
-        QN.add(q1,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 4;
-        QN.add(q2,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 5;
-        QN.add(q3,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 6;
-        QN.add(q4,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 7;
-        QN.add(q5,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 8;
-        QN.add(q6,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 9;
-        QN.add(q7,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 10;
-        QN.add(q8,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 11;
-        QN.add(q9,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 12;
-        QN.add(q10,constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 13;
-        QN.add(q11,constraints);
-
-        constraints.gridx = 0;
+        constraints.gridx = 1;
         constraints.gridy = 14;
-        QN.add(q12,constraints);
+        add(back,constraints);
+
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+
+        String x = JComboBox.getSelectedItem().toString();
+        if (x == "1")
+        {
+            count1++;
+            System.out.println("The number of 1's chosen are:" + count1);
+        }
+        else if (x == "2")
+        {
+            count2++;
+            System.out.println("The number of 2's chosen are:" + count2);
+        }
+       else if (x == "3")
+        {
+            count3++;
+            System.out.println("The number of 3's chosen are:" + count3);
+        }
 
 
-        constraints.gridx = 14;
-        constraints.gridy = 14;
-        QN.add(back,constraints);
 
-
-        add(QN);
     }
-
-
 
 
 }
