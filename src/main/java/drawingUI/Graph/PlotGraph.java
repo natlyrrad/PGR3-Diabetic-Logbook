c9e47eb3e12ccdd3a5ed0762fc8714226cd0e03a
yuuq_huang <Rayven1998@outlook.com>
2020-01-10 16:20:13 +0000

    Add dates compare UI

--- src/main/java/drawingUI/Graph/PlotGraph.java
+++ src/main/java/drawingUI/Graph/PlotGraph.java
@@ -126,10 +126,10 @@ public class PlotGraph extends ApplicationFrame {
                 String datetime= rs.getString(2);
                 int bsl = rs.getInt(3);
 
-                SimpleDateFormat format = new SimpleDateFormat("yyyy-dd-MM HH:mm:ss.S");
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
                 java.util.Date dt = null;
                 dt = format.parse(datetime);
-                if(dt.after(start) &&! dt.before(end)==true){
+                if(! dt.before(start) &&! dt.after(end)==true){
                     dataset.addValue(bsl,"blood sugar level",dt);
                 }
 
